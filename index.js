70a7e538a44220ee2cf16ba6e8cc66b56b3b0b7e
Jake <jakepyne@gmail.com>
2016-06-29 13:27:54 +0200

    Fix urls of type ../

--- index.js
+++ index.js
@@ -11,28 +11,28 @@ module.exports = function(res, debug){
     var debug = (debug != undefined) ? debug : false;
     var parseUrl = url.parse(res.request.url);
     var absolteUrl = parseUrl.protocol+"//"+parseUrl.host;
-        
+    var pathName = parseUrl.pathname;
     if(res.status == 200){
         var $ = cheerio.load(res.text);
         
         $("a").each(function(){//Change links
             var href = $(this).attr("href");
-            $(this).attr("href", replacelink(href, absolteUrl, debug));
+            $(this).attr("href", replacelink(href, absolteUrl, pathName, debug));
         });
         
         $("img").each(function(){//Change images
             var src = $(this).attr("src");
-            $(this).attr("src", replacelink(src, absolteUrl, debug));
+            $(this).attr("src", replacelink(src, absolteUrl, pathName, debug));
         });
         
         $("link").each(function(){//Change css
             var href = $(this).attr("href");
-            $(this).attr("href", replacelink(href, absolteUrl, debug));
+            $(this).attr("href", replacelink(href, absolteUrl, pathName, debug));
         });
         
         $("script").each(function(){//Change scripts
             var src = $(this).attr("src");
-            $(this).attr("src", replacelink(src, absolteUrl, debug));
+            $(this).attr("src", replacelink(src, absolteUrl, pathName, debug));
         });
         
         res.text = $.html();
@@ -42,7 +42,7 @@ module.exports = function(res, debug){
     return res;
 };
 
-function replacelink(path, absolteUrl, debug){
+function replacelink(path, absolteUrl, pathName, debug){
     if(typeof path == "string"){
         if(path.substr(0, 1) == "/" && path.substr(0, 2) != "//"){
             var absolutePath = absolteUrl + path;
@@ -52,15 +52,13 @@ function replacelink(path, absolteUrl, debug){
             return absolutePath;
         } else if(path.substr(0, 3) == "../"){
             var parseUrl = url.parse(absolteUrl);
-            var parseHost = parseUrl.host.split("/");
-            var absolutePath = parseUrl.protocol+"//";
-
-            for(var i = 0; i < parseHost.length-1; i++)
-                absolutePath += parseHost[i]+"/";
+            var parsePath = pathName.split("/");
+            var absolutePath = absolteUrl;
+            for(var i = 0; i < parsePath.length-2; i++)
+              absolutePath += parsePath[i]+"/";
 
             absolutePath += path.replace("../", "");
-
-            return absolutePath;        
+            return absolutePath;
         } else if(path.substr(0, 4) != "http" && path.substr(0, 2) != "//"){
             var absolutePath = absolteUrl + "/" + path;
             return absolutePath;
